9a65257f9a43ed3aa84ee8763cdba54c51d3d032
zakaria <zakaria.elaissaoui.insa@gmail.com>
2022-08-02 10:08:07 +0200

    added styles support

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from 'rollup-plugin-postcss'
 
 const packageJson = require("./package.json");
 
@@ -23,13 +24,15 @@ const config = [
     plugins: [
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json', tslib: require('tslib'), typescript: require('typescript') }),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss()
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.less$/, /\.css$/]
   },
 ];
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,13 @@
 
 import React from 'react';
+import './Button.less';
 
 export interface ButtonProps {
     label: string;
 }
 export const Button = ({ label }: ButtonProps) => {
 
-    return  <button>{ label } </button>
+    return  <button className='button'>{ label } </button>
 }
 
 // export default Button;
